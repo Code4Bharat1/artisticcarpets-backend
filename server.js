01749a1a50798134affe89fc240ac17797272000
karan <145835551+KaranKanakarajan@users.noreply.github.com>
2026-07-16 14:59:06 +0530

    created server

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running on port 3000");
+  res.send("Server is running on port 3000");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
